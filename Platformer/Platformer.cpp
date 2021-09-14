7004c6aa6fb4469495257b7d9351de9081286505
Thomas Maikon <thomasmaikon@hotmail.com>
2021-09-14 16:38:18 -0300

    modificando nome do jogo

--- Platformer/Platformer.cpp
+++ Platformer/Platformer.cpp
@@ -29,7 +29,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     engine->window->Mode(WINDOWED);
     engine->window->Size(600, 300);
     engine->window->Color(150, 200, 230);
-    engine->window->Title("Amazonas Em chamas");
+    engine->window->Title("Arara Juba");
     engine->window->Icon(IDI_ICON);
     //engine->window->Cursor(IDC_CURSOR);
     //engine->graphics->VSync(true);
